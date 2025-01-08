43f03000d69479b20af7acd1c65d71fb60c5be99
sagar-mmnovatech <159879684+sagar-mmnovatech@users.noreply.github.com>
2025-01-08 22:03:55 +0530

    adding templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,32 +51,46 @@
     <div class="container-fluid p-0 mb-5">
         <div class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="img/1-ielts.jpg" alt="">
+                <img class="img-fluid" src="img/ie3.png" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
                                 <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Best Online Courses</h5>
-                                <h1 class="display-3 text-white animated slideInDown">Start your future with IELTS</h1>
+
                                 <p class="fs-5 text-white mb-4 pb-2">IELTS is the world’s most popular English language test for higher education and global migration.</p>
-                                <a href="/success" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
-                                <a href="{{route('sign-in')}}" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Sign In</a>
+                                <!-- Call Button with Icon -->
+                                <a href="tel:+9540862023" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">
+                                    <i class="fa fa-phone"></i> Call Now
+                                </a>
+
+                                <!-- WhatsApp Button with Icon -->
+                                <a href="https://wa.me/9540862023?text=Hi! I have a question." target="_blank" class="btn btn-dark py-md-3 px-md-5 animated slideInRight">
+                                    <i class="fab fa-whatsapp" ></i> Message on WhatsApp
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="img/2-ielts.jpg" alt="">
+                <img class="img-fluid" src="img/ie1.png" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
                                 <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Flexible Batch and Online Training</h5>
-                                <h1 class="display-3 text-white animated slideInDown">Studies can be fun too, IELTS Made its easy.</h1>
+
                                 <p class="fs-5 text-white mb-4 pb-2">Great Tutors. Right Results! Get Desired Score in IELTS Exam.</p>
-                                <a href="/success" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
-                                <a href="{{route('sign-in')}}" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Sign In</a>
+                                <!-- Call Button with Icon -->
+                                <a href="tel:+9540862023" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">
+                                    <i class="fa fa-phone"></i> Call Now
+                                </a>
+
+                                <!-- WhatsApp Button with Icon -->
+                                <a href="https://wa.me/9540862023?text=Hi! I have a question." target="_blank" class="btn btn-dark py-md-3 px-md-5 animated slideInRight">
+                                    <i class="fab fa-whatsapp" ></i> Message on WhatsApp
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -187,36 +201,36 @@
     <div class="container-xxl py-5 category">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Categories</h6>
-                <h1 class="mb-5">Courses Categories</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Modules</h6>
+                <h1 class="mb-5">IELTS Modules</h1>
             </div>
             <div class="row g-3">
                 <div class="col-lg-7 col-md-6">
                     <div class="row g-3">
                         <div class="col-lg-12 col-md-12 wow zoomIn" data-wow-delay="0.1s">
                             <a class="position-relative d-block overflow-hidden" href="">
-                                <img class="img-fluid" src="img/cat-1.jpg" alt="">
+                                <img class="img-fluid" src="img/wr.jpg" alt="">
                                 <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
-                                    <h5 class="m-0">IELTS Vocabulary</h5>
-                                    <small class="text-primary">49 Courses</small>
+                                    <h5 class="m-0">IELTS Writing</h5>
+                                    <small class="text-primary">1 Month Program</small>
                                 </div>
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.3s">
                             <a class="position-relative d-block overflow-hidden" href="">
-                                <img class="img-fluid" src="img/cat-2.jpg" alt="">
+                                <img class="img-fluid" src="img/re.jpg" alt="">
                                 <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
-                                    <h5 class="m-0">Lessons, Strategies & Tips</h5>
-                                    <small class="text-primary">49 Courses</small>
+                                    <h5 class="m-0">IELTS Reading</h5>
+                                    <small class="text-primary">1 Month Program</small>
                                 </div>
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.5s">
                             <a class="position-relative d-block overflow-hidden" href="">
-                                <img class="img-fluid" src="img/cat-3.jpg" alt="">
+                                <img class="img-fluid" src="img/listen.jpg" alt="">
                                 <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
-                                    <h5 class="m-0">Practice Tests</h5>
-                                    <small class="text-primary">49 Courses</small>
+                                    <h5 class="m-0">IELTS Listening</h5>
+                                    <small class="text-primary">1 Month Program</small>
                                 </div>
                             </a>
                         </div>
@@ -224,10 +238,10 @@
                 </div>
                 <div class="col-lg-5 col-md-6 wow zoomIn" data-wow-delay="0.7s" style="min-height: 350px;">
                     <a class="position-relative d-block h-100 overflow-hidden" href="">
-                        <img class="img-fluid position-absolute w-100 h-100" src="img/cat-4.jpg" alt="" style="object-fit: cover;">
+                        <img class="img-fluid position-absolute w-100 h-100" src="img/sp.jpg" alt="" style="object-fit: cover;">
                         <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin:  1px;">
-                            <h5 class="m-0">Sample Essay</h5>
-                            <small class="text-primary">49 Courses</small>
+                            <h5 class="m-0">IELTS Speaking</h5>
+                            <small class="text-primary">1 Month Program</small>
                         </div>
                     </a>
                 </div>
